187377a3ab2cdd3aed78d8187872ff451b48ad6e
Valentin Leguizamon <valentin.leguizamon.sevilla@gmail.com>
2021-03-17 18:00:01 -0300

    Pagina home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Pages
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IndexComponent } from './pages/index/index.component';
+import { NoticiaComponent } from './pages/noticia/noticia.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { SearchComponent } from './pages/search/search.component';
+
+
+const routes: Routes = [
+  { path: "index", component: IndexComponent },
+  { path: ":empresa/home", component: HomeComponent } ,
+  { path: ":empresa/noticia/:id", component: NoticiaComponent },
+  { path: ":empresa/search", component: SearchComponent },
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: "**", component: Page404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { NoticiaComponent } from './pages/noticia/noticia.component';
+import { IndexComponent } from './pages/index/index.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component,
+    HomeComponent,
+    SearchComponent,
+    NoticiaComponent,
+    IndexComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

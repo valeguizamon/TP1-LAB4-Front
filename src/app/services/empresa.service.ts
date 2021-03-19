418df28f43691ba60729040521de9dec8a5d71b5
Valentin Leguizamon <valentin.leguizamon.sevilla@gmail.com>
2021-03-19 18:04:16 -0300

    Agrego los servicios para noticia y empresa

---
+++ src/app/services/empresa.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Empresa } from '../models/empresa'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpresaService {
+
+  direccion : string = '';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(){
+    this.http.get(this.direccion);
+  }
+
+  getOne(id:number){
+    this.http.get(this.direccion + id);
+  }
+
+  post(empresa:Empresa){
+    this.http.post(this.direccion, empresa);
+  }
+
+  put(id:number, empresa:Empresa){
+    this.http.post(this.direccion + id, empresa);
+  }
+
+  delete(id:number){
+    this.http.delete(this.direccion + id);
+  }
+
+}
+
---
+++ src/app/services/noticia.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Noticia } from '../models/noticia'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoticiaService {
+
+  direccion : string = '';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(){
+    this.http.get(this.direccion);
+  }
+
+  getOne(id:number){
+    this.http.get(this.direccion + id);
+  }
+
+  post(noticia:Noticia){
+    this.http.post(this.direccion, noticia);
+  }
+
+  put(id:number, noticia:Noticia){
+    this.http.post(this.direccion + id, noticia);
+  }
+
+  delete(id:number){
+    this.http.delete(this.direccion + id);
+  }
+  
+}

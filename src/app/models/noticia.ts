18e917c1752aaa41e13777254d4a65ca6938cd03
Valentín Leguizamón <valentin.leguizamon.sevilla@gmail.com>
2021-03-23 00:43:22 +0100

    Pequeñas refactorizaciones y correcciones en el modelo de notica

--- src/app/models/noticia.ts
+++ src/app/models/noticia.ts
@@ -1,12 +1,12 @@
 export interface Noticia {
 
-    id : string,
+    id : number,
     titulo: string,
     resumen: string,
     imagenNoticia: string,
     contenidoHtml: string,
     publicada: boolean,
     fecha: Date, // Fecha en la que se publica la noticia.
-    Empresa : {id : string},
+    Empresa : {id : number},
 
 }
--- src/app/services/empresa.service.ts
+++ src/app/services/empresa.service.ts
@@ -11,9 +11,7 @@ export class EmpresaService {
 
   private direccion: string = 'http://localhost:8080/api/v1/empresas/';
 
-
   constructor(private http: HttpClient) { }
-
 
   public getAll(): Observable<Empresa[]>{
     return this.http.get<Empresa[]>(this.direccion);
@@ -34,4 +32,5 @@ export class EmpresaService {
   public delete(id:number): any{
     return this.http.delete<any>(`${this.direccion+id}`);
   }
+  
 }

c07e63390891e3e69d351f799e96920a04ad1bc0
Valentín Leguizamón <valentin.leguizamon.sevilla@gmail.com>
2021-03-23 13:04:41 +0100

    Sigo trabajando en el abm noticia y su modal

--- src/app/crud/noticia/modal-noticia/modal-noticia.component.ts
+++ src/app/crud/noticia/modal-noticia/modal-noticia.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Noticia } from 'src/app/models/noticia';
+import { NoticiaService } from 'src/app/services/noticia.service';
 
 @Component({
   selector: 'modal-noticia',
   templateUrl: './modal-noticia.component.html',
   styleUrls: ['./modal-noticia.component.css']
 })
-export class ModalNoticiaComponent implements OnInit {
+export class ModalNoticiaComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @ViewChild('btnClose') btnClose : ElementRef;
+  @Input() item: Noticia;
+
+  public notiForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private noticiaSvc: NoticiaService) {
+    this.buildFormNot();
+  }
+
+  ngOnChanges(): void {
+    this.buildFormNot(this.item);
+  }
 
   ngOnInit(): void {
+  }
+
+  private buildFormNot(noti: Noticia = null): void {
+    if(noti){
+      this.notiForm.patchValue({
+        titulo: noti.titulo,
+        resumen: noti.resumen,
+        imagenNoti: noti.imagenNoticia,
+        contenidoHtml: noti.contenidoHtml,
+        publicada: noti.publicada,
+        fecha: noti.fecha,
+      });
+    } else {
+      this.notiForm = this.formBuilder.group({
+        titulo: ['', Validators.required],
+        resumen: ['', Validators.required],
+        imagenNot: ['', Validators.required],
+        contenidoHtml: ['', Validators.required],
+        publicada: ['', Validators.required],
+        fecha: ['', Validators.required],
+      });
+    }
+  }
+
+  public onSave(e: Event): void {
+    e.preventDefault();
+    console.log(this.notiForm.value);
+    if(this.item && this.item.id) {
+      this.noticiaSvc.put(this.notiForm.value, this.item.id).subscribe(data=>console.log(data));
+    } else {
+      this.noticiaSvc.post(this.notiForm.value).subscribe(data=>console.log(data));
+    }
+    this.closeModal();
+  }
+
+  public closeModal(): void {
+    this.item = null;
+    this.resetForm();
+    this.btnClose.nativeElement.click();
+  }
+
+  public resetForm(): void {
+    this.notiForm.reset();
   }
 
 }
